78d243df6d434efcecce6a5b86833988fdb74f47
Tjoosten <Tjoosten3@gmail.com>
2015-08-20 12:57:06 +0200

    Clean up config

--- config.php
+++ config.php
@@ -1,18 +1,18 @@
 <?php
 
-/*
-* Possible dates between 14-12-2014 and 12-12-2015
-* Possible traintypes: IC, ICE, L, P, TGV, THA, TRN and EXT
-* Possible languages: Dutch, English, French and German (currently only Dutch has been tested)
-* Always leave one language uncommented
-*/
-return array(
+/**
+ * Possible dates between 14-12-2014 and 12-12-2015
+ * Possible traintypes: IC, ICE, L, P, TGV, THA, TRN and EXT
+ * Possible languages: Dutch, English, French and German (currently only Dutch has been tested)
+ * Always leave one language uncommented
+ */
+return [
     'start_date' => '01-01-2015',
     'end_date' => '14-12-2015',
     'feed_version' => '1.0',
-    'shortNames' => array("IC", "ICE", "L", "P", "TGV", "THA", "TRN", "EXT"),
+    'shortNames' => ["IC", "ICE", "L", "P", "TGV", "THA", "TRN", "EXT"],
     'language' => 'nl' // Dutch
     // 'language' => 'en' // English
     // 'language' => 'fr' // French
     // 'language' => 'de' // German
-);
+];
